b44e5fec57aeb805ea1ff42fd601bbf92916fe9c
Twinkle-Dot <rmmzale@outlook.com>
2022-07-15 11:01:31 +0300

    scaffolding of a web app to search countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, Fragment } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  // Fetch countries
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  console.log(countries);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputField">
+        <input
+          type="search"
+          placeholder="Search Country..."
+          onChange={(event) => {
+            setSearchField(event.target.value.toLowerCase());
+            console.log(searchField);
+          }}
+        />
+      </div>
+      <div className="cards">
+        {countries
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(searchField)
+          )
+          .map((country) => (
+            <Fragment key={country.name.common}>
+              <div className="card-content-container">
+                <div className="img-container">
+                  <img
+                    alt={`${country.name.common}'s flag`}
+                    src={country.flags.png}
+                    className="flag-img"
+                  />
+                </div>
+                <div className="text-container">
+                  <h3>{country.name.common}</h3>
+                  <h3>Capital City: {country.capital}</h3>
+                  <h3>Population: {country.population}</h3>
+                  <h3>
+                    Languages:{" "}
+                    {country.languages
+                      ? Object.values(country.languages).join(", ")
+                      : null}
+                  </h3>
+                </div>
+              </div>
+            </Fragment>
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
