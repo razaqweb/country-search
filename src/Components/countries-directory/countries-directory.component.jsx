7763f74b0023d091e53024d9a82e8431da76b4dc
Twinkle-Dot <rmmzale@outlook.com>
2022-07-15 20:14:32 +0300

    minor fixes

--- src/Components/countries-directory/countries-directory.component.jsx
+++ src/Components/countries-directory/countries-directory.component.jsx
@@ -1,7 +1,9 @@
 import Card from "../card/card.component";
+
 const CountriesDirectory = ({ countries, searchField }) => {
   return (
     <div className="cards">
+      {/*<Card countries={countries} searchField={searchField} />*/}
       <Card countries={countries} searchField={searchField} />
     </div>
   );
